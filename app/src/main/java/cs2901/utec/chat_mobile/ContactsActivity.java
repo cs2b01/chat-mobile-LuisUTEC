75913948daccc60e314426e5a56a0bca249072d7
LuisUTEC <luis.cordero@utec.edu.pe>
2019-07-03 22:13:59 -0500

    .

--- app/src/main/java/cs2901/utec/chat_mobile/ContactsActivity.java
+++ app/src/main/java/cs2901/utec/chat_mobile/ContactsActivity.java
@@ -44,7 +44,7 @@ public class ContactsActivity extends AppCompatActivity {
     }
 
     public void getUsers(){
-        String url = "http://10.0.2.2:8000/users";
+        String url = "http://10.0.2.2:8080/users";
         RequestQueue queue = Volley.newRequestQueue(this);
         Map<String, String> params = new HashMap();
         JSONObject parameters = new JSONObject(params);
